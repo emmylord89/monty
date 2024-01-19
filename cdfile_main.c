51b9464d0f6d305904195a946ea5b895a94def1c
emmylord89 <pemmy80s@gmail.com>
2024-01-19 04:53:32 +0000

    First commit

--- cdfile_main.c
+++ cdfile_main.c
@@ -1,21 +1,24 @@
 #include "monty.h"
-#include <stdio.h>
+#define _GNU_SOURCE
+
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
 * Return: 0 on success
 */
+
 int main(int argc, char *argv[])
 {
+
 	char *content;
 	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
-
 
 	if (argc != 2)
 	{
@@ -29,6 +32,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	while (read_line > 0)
 	{
 		content = NULL;
